93d0040a6750eeb21f1bd6fd7ad5330deaab6b9c
DarrelFW321 <darrelwihandi123@gmail.com>
2025-01-02 04:23:31 -0500

    hellp

--- api.py
+++ api.py
@@ -9,18 +9,18 @@ CORS(app)  # Enable CORS for cross-origin requests
         
 @app.route('/', methods=['GET'])
 def lebron_stats():
-    # career = playercareerstats.PlayerCareerStats(player_id=2544)
-    # data = career.get_dict()
+    career = playercareerstats.PlayerCareerStats(player_id=2544)
+    data = career.get_dict()
 
-    # # Get career total points
-    # regular_season_career = data['resultSets'][1]
-    # regular_season_points = regular_season_career['rowSet'][0][23]  # Points
-    # postseason_career = data['resultSets'][3]
-    # postseason_points = postseason_career['rowSet'][0][23]
+    # Get career total points
+    regular_season_career = data['resultSets'][1]
+    regular_season_points = regular_season_career['rowSet'][0][23]  # Points
+    postseason_career = data['resultSets'][3]
+    postseason_points = postseason_career['rowSet'][0][23]
 
     return jsonify({
         "player": "LeBron James",
-        "career_points": "300" #regular_season_points + postseason_points
+        "career_points": regular_season_points + postseason_points
     })
 
 if __name__ == '__main__':
