c4458362927ee78e62c14216abd5aeddf6960d18
DarrelFW321 <60972474+DarrelFW321@users.noreply.github.com>
2025-01-02 02:07:37 -0500

    Add files via upload

---
+++ api.py
@@ -0,0 +1,30 @@
+from nba_api.stats.static import players
+from nba_api.stats.endpoints import playercareerstats
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS for cross-origin requests
+
+@app.route('/api/lebron_stats', methods=['GET'])
+def lebron_stats():
+    # Fetch LeBron James' stats
+    player = players.find_players_by_full_name('LeBron James')[0]
+    player_id = player['id']
+
+    career = playercareerstats.PlayerCareerStats(player_id=player_id)
+    data = career.get_dict()
+
+    # Get career total points
+    regular_season_career = data['resultSets'][1]
+    regular_season_points = regular_season_career['rowSet'][0][23]  # Points
+    postseason_career = data['resultSets'][3]
+    postseason_points = postseason_career['rowSet'][0][23]
+
+    return jsonify({
+        "player": "LeBron James",
+        "career_points": regular_season_points + postseason_points
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
